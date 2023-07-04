34b9a4518d0be30b0ddf5dfb58762d3387b79f67
Shivanshu Gupta <77326511+Shivanshugupta007@users.noreply.github.com>
2023-07-04 23:02:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.use(express.json());
+
 dotenv.config({ path: './config.env' });
 
 const PORT = process.env.PORT;
 
 require('./db/conn');
-
-app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'build')));
 
